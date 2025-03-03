e86956c3851a7bd49909e4e0d9cf09909167e12b
josenerydev <josenerydev@gmail.com>
2025-03-03 11:07:43 -0300

    feat(config): adiciona classes para gerenciar configurações
    Introduz as classes `ConfigurationExtensions` e `ConfigurationKeys` para facilitar a recuperação de strings de conexão do banco de dados e Redis a partir de configurações ou variáveis de ambiente. Remove referências a uma pasta desnecessária no arquivo de projeto (`.csproj`).

---
+++ src/DotNetOnion.Template.CrossCutting.Utils/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DotNetOnion.Template.CrossCutting.Utils.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        public static string GetDatabaseConnectionString(this IConfiguration configuration)
+        {
+            return configuration.GetRequiredConfiguration(ConfigurationKeys.DefaultConnectionEnv, ConfigurationKeys.DefaultConnectionConfig);
+        }
+
+        public static string GetRedisConnectionString(this IConfiguration configuration)
+        {
+            return configuration.GetRequiredConfiguration(ConfigurationKeys.RedisConnectionEnv, ConfigurationKeys.RedisConnectionConfig);
+        }
+
+        private static string GetRequiredConfiguration(this IConfiguration configuration, string envVariable, string configKey)
+        {
+            var value = Environment.GetEnvironmentVariable(envVariable)
+                        ?? configuration.GetValue<string>(configKey);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new InvalidOperationException($"Configuração obrigatória ausente: '{configKey}' ou variável de ambiente '{envVariable}'");
+            }
+
+            return value;
+        }
+    }
+}
---
+++ src/DotNetOnion.Template.CrossCutting.Utils/Configuration/ConfigurationKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetOnion.Template.CrossCutting.Utils.Configuration
+{
+    public static class ConfigurationKeys
+    {
+        public const string DefaultConnectionEnv = "ConnectionStrings__DefaultConnection";
+        public const string DefaultConnectionConfig = "ConnectionStrings:DefaultConnection";
+
+        public const string RedisConnectionEnv = "ConnectionStrings__RedisConnection";
+        public const string RedisConnectionConfig = "ConnectionStrings:RedisConnection";
+    }
+}
